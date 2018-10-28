c757c0307431977e5e33c0a73521058963b94381
mheaney14 <mheaney14@gmail.com>
2018-10-28 15:52:57 -0600

    dasm syntax
    updated syntax of program for dasm

--- test1.asm
+++ test1.asm
@@ -1,14 +1,65 @@
-BCPL
-tokenized
-	processor 6502
-	org $1100
+
+
 	
-	dc.w	end
-	dc.w	#1234
-	dc.b $9e, " ", 0
+	;.segment	"BASIC"
+	processor 6502
+	org $1001	
+;	.word	RUN
+	
+;RUN:	.word	@end
+
+	dc.w basicend
+	dc.w 2010
+	dc.b $9e
+	
+	dc.a " "
+	dc.a "4110"
+	dc.b 0
+	
+	
+;	.byte	<(MAIN / 1000 .mod 10) + $30
+;	.byte	<(MAIN / 100 .mod 10) + $30
+;	.byte	<(MAIN / 10 .mod 10) + $30
+;	.byte	<(MAIN / 1 .mod 10) + $30
+;	.byte	0		
+
+	
+;	.segment "STARTUP"
+
+basicend	
+		dc.w 0
+
+
+
+MAIN:	
+	jsr	$e55f
+	lda	#'H	;H
+	jsr	$ffd2
+	lda	#'E	;E
+	jsr	$ffd2
+	lda	#'L	;L
+	jsr	$ffd2
+	lda	#'L	;L
+	jsr	$ffd2
+	lda	#'O	;O
+	jsr	$ffd2
+	lda	#' 	;space
+	jsr	$ffd2
+	lda	#'W	;W
+	jsr	$ffd2
+	lda	#'O	;O
+	jsr	$ffd2
+	lda	#'R	;R
+	jsr	$ffd2
+	lda	#'L	;L
+	jsr	$ffd2
+	lda	#'D	;D
+	jsr	$ffd2
+	lda	#'!	;!
+	jsr	$ffd2
 	
 end:
-	dc.w 0
+	jmp end
 	
 start:
 	rts
