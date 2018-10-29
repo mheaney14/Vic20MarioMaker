0ca18d97a8ba00808d259812dafd6117a11c5149
mheaney14 <mheaney14@gmail.com>
2018-10-28 21:11:42 -0600

    comments/stand-alone
    Added comments and created stand-alone stub file

---
+++ stub.asm
@@ -0,0 +1,7 @@
+	dc.w basicend
+	dc.w 1234			;line number for sys instruction (arbitrary?)
+	dc.b $9e, "4109"	;sys instruction and memory location
+	dc.b 0				; end of instruction
+basicend	
+		dc.w 0	;end of basic program
+
--- test1.asm
+++ test1.asm
@@ -4,9 +4,9 @@
 
 
 	dc.w basicend
-	dc.w 1234
-	dc.b $9e, "4109"	;sys instruction
-	dc.b 0
+	dc.w 1234			;line number for sys instruction (arbitrary?)
+	dc.b $9e, "4109"	;sys instruction and memory location
+	dc.b 0				; end of instruction
 basicend	
 		dc.w 0	;end of basic program
 
