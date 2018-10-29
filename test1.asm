6008b4c48455789a7a4517d0cb43ba58cbd427e4
mheaney14 <mheaney14@gmail.com>
2018-10-28 21:07:29 -0600

    clean-up
    removed commented out code

--- test1.asm
+++ test1.asm
@@ -1,38 +1,12 @@
 
-
-	
-	;.segment	"BASIC"
 	processor 6502
 	org $1001	
-;	.word	RUN
-	
-;RUN:	.word	@end
+
 
 	dc.w basicend
-;	dc.w 1234
-
-	
-	
-
-;	dc.w " "	;space between sys and memory location (?)
 	dc.w 1234
 	dc.b $9e, "4109"	;sys instruction
-	;dc.w 4361	;memory address where assembly starts (org + size of everthing loaded)
-	;dc.w 4107
 	dc.b 0
-	
-;	dc.w 0		;0 to terminate instruction
-	
-	
-;	.byte	<(MAIN / 1000 .mod 10) + $30
-;	.byte	<(MAIN / 100 .mod 10) + $30
-;	.byte	<(MAIN / 10 .mod 10) + $30
-;	.byte	<(MAIN / 1 .mod 10) + $30
-;	.byte	0		
-
-	
-;	.segment "STARTUP"
-
 basicend	
 		dc.w 0	;end of basic program
 
@@ -66,5 +40,6 @@ MAIN:
 	jsr	$ffd2
 	
 end:
+	jmp end
 	rts
 	
