ff2c0ae3cd062ee839191cfd58ecd9bc464ba747
qhnguyen <qhnguyen@zone08-wc.cs.ucalgary.ca>
2018-10-29 13:58:52 -0600

    merge with Graham

--- hello.asm
+++ hello.asm
@@ -11,12 +11,186 @@ CHRIN	= $FFE4
 basicend	
 		dc.w 0	;end of basic program
 		
+
+;	jsr $e55f						; clear screen
+	lda #28
+	sta 52
+	lda #28
+	sta 56			; the above 4 lines cause memory not to be used for code (used for storing 64 new characters)
+	
+	; this stores the regular set of characters, we can then swap new ones in their places
+	; for x = $0 to $199 get from $8000+x, store at $1C00+x
+	ldx $0
+loadChar:
+	lda $8000,X		; load x from ROM
+	sta $1C00,X		; store previous char in RAM
+	inx				; increment x
+	cpx $200		; if x < $200 then do loop again
+	bne loadChar
+	
+	ldx $9005
+	lda #$FF
+	sta $9005		; characters are stored at $1C00 - $1DFF and use 8 bytes of memory each
+
+;******************* Start of Graphics *******************
+; characters are 8 bytes
+; each byte in binary represents a line of the char (1 => pixel on, 0 => pixel off)
+
+;******************* Mario Character *******************
+	lda #$3C		; start of mario character (top of head)
+	sta $1C00		; start of memory location for mario (mario replaces '@ => get mario in accumulator: lda #'@)
+	lda #$3C
+	sta $1C01
+	lda #$18
+	sta $1C02
+	lda #$FF
+	sta $1C03
+	lda #$3C
+	sta $1C04
+	lda #$3C
+	sta $1C05
+	lda #$24
+	sta $1C06
+	lda #$C3		; feet
+	sta $1C07		; end memory location for mario
+	
+;******************* Koopa 1 Character *******************
+	lda #$02		; replaces '[
+	sta $1CD8
+	lda #$36
+	sta $1CD9
+	lda #$36
+	sta $1CDA
+	lda #$1C
+	sta $1CDB
+	lda #$70
+	sta $1CDC
+	lda #$10
+	sta $1CDD
+	lda #$28
+	sta $1CDE
+	lda #$28
+	sta $1CDF
+	
+;******************* Koopa 2 Character *******************
+	lda #$30		; replaces ']
+	sta $1CE8
+	lda #$37
+	sta $1CE9
+	lda #$1E
+	sta $1CEA
+	lda #$70
+	sta $1CEB
+	lda #$10
+	sta $1CEC
+	lda #$28
+	sta $1CED
+	lda #$28
+	sta $1CEE
+	lda #$00
+	sta $1CEF
+	
+;******************* Goomba *******************
+	lda #$00		; replaces '"
+	sta $1D10
+	lda #$00
+	sta $1D11
+	lda #$18
+	sta $1D12
+	lda #$3C
+	sta $1D13
+	lda #$7E
+	sta $1D14
+	lda #$7E
+	sta $1D15
+	lda #$18
+	sta $1D16
+	lda #$24
+	sta $1D17
+	
+;******************* Box *******************
+	lda #$FF		; replaces '# can be used as selector
+	sta $1D18
+	lda #$81
+	sta $1D19
+	lda #$81
+	sta $1D1A
+	lda #$81
+	sta $1D1B
+	lda #$81
+	sta $1D1C
+	lda #$81
+	sta $1D1D
+	lda #$81
+	sta $1D1E
+	lda #$FF
+	sta $1D1F
+	
+;******************* Block *******************
+	lda #$FF		; replaces '&
+	sta $1D30
+	lda #$FF
+	sta $1D31
+	lda #$FF
+	sta $1D32
+	lda #$FF
+	sta $1D33
+	lda #$FF
+	sta $1D34
+	lda #$FF
+	sta $1D35
+	lda #$FF
+	sta $1D36
+	lda #$FF
+	sta $1D37
+	
+;******************* Coin Block *******************
+	lda #$FF		; replaces '$
+	sta $1D20
+	lda #$81
+	sta $1D21
+	lda #$99
+	sta $1D22
+	lda #$B5
+	sta $1D23
+	lda #$AD
+	sta $1D24
+	lda #$99
+	sta $1D25
+	lda #$81
+	sta $1D26
+	lda #$FF
+	sta $1D27
+
+;******************* Question Block *******************
+	lda #$FF		; replaces '?
+	sta $1DF8
+	lda #$81
+	sta $1DF9
+	lda #$9D
+	sta $1DFA
+	lda #$A5
+	sta $1DFB
+	lda #$89
+	sta $1DFC
+	lda #$81
+	sta $1DFD
+	lda #$91
+	sta $1DFE
+	lda #$FF
+	sta $1DFF
+
+;******************* End of Graphics *******************
+
+	; jsr $e55f		; clear screen
 start:		
 	jsr $E55F
-    lda #8        ; screen and border colors
-    sta 36879
+    lda #0        ; screen and border colors
+    sta $900F
+	lda #1			;Set the background of the character to white
+	sta $286		;Store it in $286 memory
 
-	ldx #0                 ; clrscr
+	ldx #0
 cll:
     lda #32+128
     sta 7680,x      ; screen memory
@@ -24,16 +198,24 @@ cll:
     dex
     bne cll
 
-	ldx #21
-brick:
-	lda #3
-	sta 38400 + 0,x
-	lda #2
-	sta 38400 + 22,x
-	lda #3
-	sta 38400 + 23,x
-	lda #2
-	sta 38400 + 1,x
+	lda #'@			; load mario
+	jsr $FFD2		; print char in accumulator
+	lda #'[			; load koopa1
+	jsr $FFD2		; print char in accumulator
+	lda #']			; load koopa2
+	jsr $FFD2		; print char in accumulator
+	lda #'"			; load goomba
+	jsr $FFD2		; print char in accumulator
+	lda #'&			; load block
+	jsr $FFD2		; print char in accumulator
+	lda #'#			; load box
+	jsr $FFD2		; print char in accumulator
+	lda #'$			; load coin block
+	jsr $FFD2		; print char in accumulator
+	lda #'?			; load question block
+	jsr $FFD2		; print char in accumulator
+
+
 
 end:
 	jmp end
