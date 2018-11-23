a15b09dfd36e2bb817c49ef22cd5729e01d89a7d
graham-kelly <graham.l.kelly@gmail.com>
2018-11-22 20:19:45 -0700

    Add files via upload
    Can paste new functions in (version 1 of each function is for first half of chars, version 2 for second half). Up and down don't work yet, just an update with better debugging.

--- charDemo2.asm
+++ charDemo2.asm
@@ -166,15 +166,15 @@ set2ndChars:		; all these start initially blank
 printChars:
 	lda chars,Y
 	cmp #0			; if there is more to print
-	beq donePrg
+	beq getKeyInput
 	jsr CHROUT
 	iny
 	jmp printChars
-		
-moveUp:
-	jsr CHROUT			; HERE FOR TESTING
+
+moveMyChar:
 	ldx #$08			; HERE FOR TESTING
 	ldy #$10			; HERE FOR TESTING
+moveUp2:
 	lda GRAPHSTART2,X
 	pha			; push value at input X to stack
 	tya
@@ -182,14 +182,14 @@ moveUp:
 	txa
 	tay
 	iny			; Y = X+1
-upLoop1:
+up2Loop1:
 	lda GRAPHSTART2,Y	; load at X+1
 	sta GRAPHSTART2,X	; store at X
 	inx			; increment to move to next line down
 	iny
 	tya
-	and #$07	; stop looping when last 3 bits of y are 0
-	bne upLoop1
+	and #7		; stop looping when last 3 bits of y are 0
+	bne up2Loop1
 	pla
 	tay			; input y is back from stack
 	lda GRAPHSTART2,Y
@@ -197,24 +197,26 @@ upLoop1:
 	tya
 	tax
 	inx			; X = Y+1
-upLoop2:
+up2Loop2:
 	lda GRAPHSTART2,X	; load at Y+1
 	sta GRAPHSTART2,Y ; store at Y
 	inx			; increment to move to next line down
 	iny
 	txa
-	and #$07	; stop looping when last 3 bits of x are 0
-	bne upLoop2
+	and #7		; stop looping when last 3 bits of x are 0
+	bne up2Loop2
 	pla
 	tax
 	lda GRAPHSTART2,X
 	sta GRAPHSTART2,Y
 
+
+	
 getKeyInput:
 	lda #0
 	jsr	CHRIN
 	cmp #'W		; if w pressed 
-	beq moveUp
+	beq moveMyChar
 	jmp getKeyInput
 	
 donePrg:
@@ -227,151 +229,7 @@ chars:
 
 
 
-moveRight:		; store address of characters to move right in x and y
-	lda #$01
-	and $1C00,Y
-	lsr			; set carry bit to bit 0 of Y (automatically uses accumulator)
-	ror $1C00,X
-	txa
-	pha			; push X to stack
-	tya
-	tax			; put Y in X register
-	ror $1C00,X
-	pla
-	tax			; put X back from stack
-	inx			; move next line of character
-	iny
-	txa
-	and #$07	; if more lines need to be done (x ends with a number 1-7 or 9-F, 0/8 => done)
-	bne moveRight
-	
-moveLeft:		; store address of characters to move left in x and y
-	lda #$80
-	and $1C00,X
-	adc #$80	; set carry bit to bit 7 of X
-	txa
-	pha			; push X to stack
-	tya
-	tax			; put Y in X register
-	rol $1C00,X
-	pla
-	tax			; put X back from stack
-	rol $1C00,X
-	inx			; move next line of character
-	iny
-	txa
-	and #$07	; if more lines need to be done (x ends with a number 1-7 or 9-F, 0/8 => done)
-	bne moveLeft
-	
-moveUp:
-	lda $1C00,X
-	pha			; push value at input X to stack
-	tya
-	pha			; pushed input Y to stack
-	txa
-	tay
-	iny			; Y = X+1
-upLoop1:
-	lda $1C00,Y	; load at X+1
-	sta $1C00,X	; store at X
-	inx			; increment to move to next line down
-	iny
-	tya
-	and #7		; stop looping when last 3 bits of y are 0
-	bne upLoop1
-	pla
-	tay			; input y is back from stack
-	lda $1C00,Y
-	sta $1C00,X
-	tya
-	tax
-	inx			; X = Y+1
-upLoop2:
-	lda $1C00,X	; load at Y+1
-	sta $1C00,Y ; store at Y
-	inx			; increment to move to next line down
-	iny
-	txa
-	and #7		; stop looping when last 3 bits of x are 0
-	bne upLoop2
-	pla
-	tax
-	lda $1C00,X
-	sta $1C00,Y
-	
-moveDown:
-	lda #0
-	asl			; set carry bit to 0 (automatically uses accumulator)
-	txa
-	adc #7		; shouldn't set carry bit
-	tax			; x = input+7
-	tya
-	adc #7		;shouldn't set carry bit
-	tay			; y = input+7
-	
-	lda $1C00,Y
-	pha			; push value at input Y + 7 to stack
-	txa
-	pha			; pushed input X + 7 to stack
-	tya
-	tax
-	dex			; X = Y-1
-downLoop1:
-	lda $1C00,X	; load at Y-1
-	sta $1C00,Y	; store at Y
-	dex			; decrement to move to next line up
-	dey
-	txa
-	and #7		; stop looping when last 3 bits of y are 0
-	bne downLoop1
-	pla
-	tax			; input X+7 is back from stack
-	lda $1C00,X
-	sta $1C00,Y
-	txa
-	tay
-	dex			; Y = X-1
-downLoop2:
-	lda $1C00,Y	; load at X-1
-	sta $1C00,X ; store at X
-	dex			; decrement to move to next line up
-	dey
-	tya
-	and #7		; stop looping when last 3 bits of x are 0
-	bne downLoop2
-	pla
-	tay
-	lda $1C00,Y
-	sta $1C00,X
 
-
-
-
-
-
-
-
-
-
-
-
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	
 	
---
+++ charDemoFunctions.asm
@@ -0,0 +1,235 @@
+moveRight1:		; store address of characters to move right in x and y
+	lda #$01
+	and GRAPHSTART1,Y
+	lsr			; set carry bit to bit 0 of Y (automatically uses accumulator)
+	ror GRAPHSTART1,X
+	txa
+	pha			; push X to stack
+	tya
+	tax			; put Y in X register
+	ror GRAPHSTART1,X
+	pla
+	tax			; put X back from stack
+	inx			; move next line of character
+	iny
+	txa
+	and #$07	; if more lines need to be done (x ends with a number 1-7 or 9-F, 0/8 => done)
+	bne moveRight1
+	
+moveLeft1:		; store address of characters to move left in x and y
+	lda #$80
+	and GRAPHSTART1,X
+	adc #$80	; set carry bit to bit 7 of X
+	txa
+	pha			; push X to stack
+	tya
+	tax			; put Y in X register
+	rol GRAPHSTART1,X
+	pla
+	tax			; put X back from stack
+	rol GRAPHSTART1,X
+	inx			; move next line of character
+	iny
+	txa
+	and #$07	; if more lines need to be done (x ends with a number 1-7 or 9-F, 0/8 => done)
+	bne moveLeft1
+	
+moveUp1:
+	lda GRAPHSTART1,X
+	pha			; push value at input X to stack
+	tya
+	pha			; pushed input Y to stack
+	txa
+	tay
+	iny			; Y = X+1
+up1Loop1:
+	lda GRAPHSTART1,Y	; load at X+1
+	sta GRAPHSTART1,X	; store at X
+	inx			; increment to move to next line down
+	iny
+	tya
+	and #7		; stop looping when last 3 bits of y are 0
+	bne up1Loop1
+	pla
+	tay			; input y is back from stack
+	lda GRAPHSTART1,Y
+	sta GRAPHSTART1,X
+	tya
+	tax
+	inx			; X = Y+1
+up1Loop2:
+	lda GRAPHSTART1,X	; load at Y+1
+	sta GRAPHSTART1,Y ; store at Y
+	inx			; increment to move to next line down
+	iny
+	txa
+	and #7		; stop looping when last 3 bits of x are 0
+	bne up1Loop2
+	pla
+	tax
+	lda GRAPHSTART1,X
+	sta GRAPHSTART1,Y
+	
+moveDown1:
+	lda #0
+	asl			; set carry bit to 0 (automatically uses accumulator)
+	txa
+	adc #7		; shouldn't set carry bit
+	tax			; x = input+7
+	tya
+	adc #7		;shouldn't set carry bit
+	tay			; y = input+7
+	
+	lda GRAPHSTART1,Y
+	pha			; push value at input Y + 7 to stack
+	txa
+	pha			; pushed input X + 7 to stack
+	tya
+	tax
+	dex			; X = Y-1
+down1Loop1:
+	lda GRAPHSTART1,X	; load at Y-1
+	sta GRAPHSTART1,Y	; store at Y
+	dex			; decrement to move to next line up
+	dey
+	txa
+	and #7		; stop looping when last 3 bits of y are 0
+	bne down1Loop1
+	pla
+	tax			; input X+7 is back from stack
+	lda GRAPHSTART1,X
+	sta GRAPHSTART1,Y
+	txa
+	tay
+	dex			; Y = X-1
+down1Loop2:
+	lda GRAPHSTART1,Y	; load at X-1
+	sta GRAPHSTART1,X ; store at X
+	dex			; decrement to move to next line up
+	dey
+	tya
+	and #7		; stop looping when last 3 bits of x are 0
+	bne down1Loop2
+	pla
+	tay
+	lda GRAPHSTART1,Y
+	sta GRAPHSTART1,X
+
+;**************************************************************************************************************;
+	
+moveRight2:		; store address of characters to move right in x and y
+	lda #$01
+	and GRAPHSTART2,Y
+	lsr			; set carry bit to bit 0 of Y (automatically uses accumulator)
+	ror GRAPHSTART2,X
+	txa
+	pha			; push X to stack
+	tya
+	tax			; put Y in X register
+	ror GRAPHSTART2,X
+	pla
+	tax			; put X back from stack
+	inx			; move next line of character
+	iny
+	txa
+	and #$07	; if more lines need to be done (x ends with a number 1-7 or 9-F, 0/8 => done)
+	bne moveRight2
+	
+moveLeft2:		; store address of characters to move left in x and y
+	lda #$80
+	and GRAPHSTART2,X
+	adc #$80	; set carry bit to bit 7 of X
+	txa
+	pha			; push X to stack
+	tya
+	tax			; put Y in X register
+	rol GRAPHSTART2,X
+	pla
+	tax			; put X back from stack
+	rol GRAPHSTART2,X
+	inx			; move next line of character
+	iny
+	txa
+	and #$07	; if more lines need to be done (x ends with a number 1-7 or 9-F, 0/8 => done)
+	bne moveLeft2
+	
+moveUp2:
+	lda GRAPHSTART2,X
+	pha			; push value at input X to stack
+	tya
+	pha			; pushed input Y to stack
+	txa
+	tay
+	iny			; Y = X+1
+up2Loop1:
+	lda GRAPHSTART2,Y	; load at X+1
+	sta GRAPHSTART2,X	; store at X
+	inx			; increment to move to next line down
+	iny
+	tya
+	and #7		; stop looping when last 3 bits of y are 0
+	bne up2Loop1
+	pla
+	tay			; input y is back from stack
+	lda GRAPHSTART2,Y
+	sta GRAPHSTART2,X
+	tya
+	tax
+	inx			; X = Y+1
+up2Loop2:
+	lda GRAPHSTART2,X	; load at Y+1
+	sta GRAPHSTART2,Y ; store at Y
+	inx			; increment to move to next line down
+	iny
+	txa
+	and #7		; stop looping when last 3 bits of x are 0
+	bne up2Loop2
+	pla
+	tax
+	lda GRAPHSTART2,X
+	sta GRAPHSTART2,Y
+	
+moveDown2:
+	lda #0
+	asl			; set carry bit to 0 (automatically uses accumulator)
+	txa
+	adc #7		; shouldn't set carry bit
+	tax			; x = input+7
+	tya
+	adc #7		;shouldn't set carry bit
+	tay			; y = input+7
+	
+	lda GRAPHSTART2,Y
+	pha			; push value at input Y + 7 to stack
+	txa
+	pha			; pushed input X + 7 to stack
+	tya
+	tax
+	dex			; X = Y-1
+down2Loop1:
+	lda GRAPHSTART2,X	; load at Y-1
+	sta GRAPHSTART2,Y	; store at Y
+	dex			; decrement to move to next line up
+	dey
+	txa
+	and #7		; stop looping when last 3 bits of y are 0
+	bne down2Loop1
+	pla
+	tax			; input X+7 is back from stack
+	lda GRAPHSTART2,X
+	sta GRAPHSTART2,Y
+	txa
+	tay
+	dex			; Y = X-1
+down2Loop2:
+	lda GRAPHSTART2,Y	; load at X-1
+	sta GRAPHSTART2,X ; store at X
+	dex			; decrement to move to next line up
+	dey
+	tya
+	and #7		; stop looping when last 3 bits of x are 0
+	bne down2Loop2
+	pla
+	tay
+	lda GRAPHSTART2,Y
+	sta GRAPHSTART2,X
